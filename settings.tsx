84184b14fbf13111a0d252f0f65142d0e073f825
C.s.w. <104325729+Sol-HQ@users.noreply.github.com>
2025-05-29 15:16:13 -0700

    Update settings.tsx

--- settings.tsx
+++ settings.tsx
@@ -1,14 +1,16 @@
 // Purpose: Contains texts that will be used in the UI when rendering the buttons
 // label has to be the exact same as in your candy machine config
 export const mintText = [
-  { label: "WL", mintText: "WL mint paying with SOL", buttonLabel: "Mint now!", header: "WL Mint" },
-  { label: "OG", mintText: "Mint Paying with ABC", buttonLabel: "Mint now!", header: "OG Mint" },
-  { label: "publi", mintText: "Mint Paying with DEF", buttonLabel: "Mint now!", header: "Public Mint" },
-  { label: "default", mintText: "default mintText", buttonLabel: "Mint now!", header: "Default"}
+  { label: "OG", mintText: "JM Submit Wallet Bruh Sub/O", buttonLabel: "Mint!", header: "OG Mint .0" },
+  { label: "WL", mintText: "Submit wallet Sub/O", buttonLabel: "Mint!", header: "WL Mint .0" },
+  { label: "Public", mintText: "Mints'Live JUP", buttonLabel: "Mint now!", header: "Public Mint 1.JUP" },
+  { label: "default", mintText: "default mintText", buttonLabel: "Mint now!", header: "default"}
 ];
 
 //header image in the ui. replace with your own
-export const image = "https://avatars.githubusercontent.com/u/93528482?v=4";
+export const image = "https://gateway.irys.xyz/WUWuXRI1P_DF6RgembIW8h9ZX2tVbZ10noPYVsRJHjg?ext=png";
 
 //website title
-export const headerText = "MarkSackerberg's mint UI";
+export const headerText = "JuP.iO.";
+
+export const bottomText = "Home Web";
