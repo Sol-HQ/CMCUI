9bcce76b49ec94b1ade41a859c1d5cced18ef36f
C.s.w. <104325729+Sol-HQ@users.noreply.github.com>
2025-05-29 15:16:52 -0700

    Update allowlist.tsx

--- allowlist.tsx
+++ allowlist.tsx
@@ -1,22 +1,24 @@
 //add your allowlists here. They need to be in the same format as you used to generate your merkle root. 
 //You can also add them here and copy the route from the console log 
 export const allowLists = new Map<string, Array<string>> ([
+  ["OGs",
+    [
+      "HScJWpHByTMVQuvdMFiy9KqyXTkmyV9E2osA6qQTYvGT",
+      "5bj5sYcJQoDeykVX5wVeBaiM5HgV3q7SF1nPt75bHLjX",
+      "78FujgY7qzFx66DgSckizJGdx4advvLQXT98iWWHkvn6",
+      "5PLZSYo7NSkuX7hRXrXNP4WBxz1TiuvGdDAjfSZUKmN8",
+      "5pc9Qe6VXzUYPgzca3TFufUxpeCNFa7oph1E6rukoqMT"
+    ]],
     ["WL",
     [
-      "Tes1zkZkXhgTaMFqVgbgvMsVkRJpq4Y6g54SbDBeKVV",
-      "GjwcWFQYzemBtpUoN5fMAP2FZviTtMRWCmrppGuTthJS",
-      "AT8nPwujHAD14cLojTcB1qdBzA1VXnT6LVGuUd6Y73Cy"
+      "HScJWpHByTMVQuvdMFiy9KqyXTkmyV9E2osA6qQTYvGT",
+      "47W8Mftd359Moi8vpZbSPAXExP9EGtrLGbtdv8HJusNn"
     ]],
-    ["OG",
+    ["Public",
     [
-      "Ur1CbWSGsXCdedknRbJsEk7urwAvu1uddmQv51nAnXB",
-      "GjwcWFQYzemBtpUoN5fMAP2FZviTtMRWCmrppGuTthJS",
-      "AT8nPwujHAD14cLojTcB1qdBzA1VXnT6LVGuUd6Y73Cy",
-    ]],
-    ["default",
-    [
-      "Tes1zkZkXhgTaMFqVgbgvMsVkRJpq4Y6g54SbDBeKVV",
-      "GjwcWFQYzemBtpUoN5fMAP2FZviTtMRWCmrppGuTthJS",
-      "AT8nPwujHAD14cLojTcB1qdBzA1VXnT6LVGuUd6Y73Cy"
+      "HScJWpHByTMVQuvdMFiy9KqyXTkmyV9E2osA6qQTYvGT",
+      "5bj5sYcJQoDeykVX5wVeBaiM5HgV3q7SF1nPt75bHLjX",
+      "47W8Mftd359Moi8vpZbSPAXExP9EGtrLGbtdv8HJusNn"
     ]],
 ]);
+ 
