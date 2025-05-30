b34199d1bb39e76dec27b4de83c4fa8851f6606c
C.s.w. <104325729+Sol-HQ@users.noreply.github.com>
2025-05-29 17:33:09 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,13 +10,48 @@ import "@solana/wallet-adapter-react-ui/styles.css";
 import { ChakraProvider } from '@chakra-ui/react'
 import { image, headerText } from 'settings'
 import { SolanaTimeProvider } from "@/utils/SolanaTimeContext";
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter,
+  LedgerWalletAdapter,
+  MathWalletAdapter,
+  Coin98WalletAdapter,
+  BitpieWalletAdapter,
+  CloverWalletAdapter,
+  CoinhubWalletAdapter,
+  TrustWalletAdapter,
+  SafePalWalletAdapter,
+  NightlyWalletAdapter
+} from '@solana/wallet-adapter-wallets';
 
 export default function App({ Component, pageProps }: AppProps) {
+  let network = WalletAdapterNetwork.Devnet;
+  if (process.env.NEXT_PUBLIC_ENVIRONMENT === "mainnet-beta" || process.env.NEXT_PUBLIC_ENVIRONMENT === "mainnet") {
+    network = WalletAdapterNetwork.Mainnet;
+  }
   let endpoint = "https://api.devnet.solana.com";
   if (process.env.NEXT_PUBLIC_RPC) {
     endpoint = process.env.NEXT_PUBLIC_RPC;
   }
- return (
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter({ network }),
+      new TorusWalletAdapter(),
+      new LedgerWalletAdapter(),
+      new MathWalletAdapter(),
+      new Coin98WalletAdapter(),
+      new BitpieWalletAdapter(),
+      new CloverWalletAdapter(),
+      new CoinhubWalletAdapter(),
+      new TrustWalletAdapter(),
+      new SafePalWalletAdapter(),
+      new NightlyWalletAdapter(),
+    ],
+    [network]
+  );
+  return (
     <>
       <Head>
         <meta property="og:type" content="Creative Squares" />
@@ -26,11 +61,7 @@ export default function App({ Component, pageProps }: AppProps) {
           content="Candy Core Mint, on Solana"
         />
         <meta name="description" content="Website Based in Mark S. Frame, Custom Collection" />
-
-        <meta
-          property="og:image"
-          content={image}
-        />
+        <meta property="og:image" content={image} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>{headerText}</title>
         <link rel="icon" href="/favicon.ico" />
